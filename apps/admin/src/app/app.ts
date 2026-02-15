e2adaabb2caf99a4bef7222da3d0d1970b2a68e6
john conte <information9527@gmail.com>
2026-02-15 19:32:16 +0800

    feat: Implement initial app components for frontend and admin applications, including navigation and basic authentication integration.

--- apps/admin/src/app/app.ts
+++ apps/admin/src/app/app.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { AuthService } from './services/auth';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [CommonModule, NxWelcome, RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   protected title = 'admin';
+  public currentUser$ = this.authService.currentUser$;
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
